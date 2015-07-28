8d78060698e28db4a1071f3b55d2507348553343
KaiyoteSoft <Spikeyhedgehog141@gmail.com>
2015-07-27 17:34:55 -0700

    Added 25 states and brought json data from a server

--- parser.js
+++ parser.js
@@ -1,20 +1,51 @@
-function stateOutput(state) {
-	// document.write("Hello world " + state);
-	// var colonies = us_beedata[0].colonies;
-	var i = 0;
-	state_name = state;
-	for (i; i < us_beedata.length; i++){
-		if (us_beedata[i].state == state_name) {
-			var colonies = us_beedata[i].colonies;
-			var loss = us_beedata[i].loss;
+function processData(state) {
+	var jdata;
+	jQuery.ajax({
+		url: "https://bees.apispark.net/v1/data/",
+		type: "GET",
+		contentType: 'application/json; charset=utf-8',
+		success: function(resultData) {
+			//this is where your json is processed
+		jdata = resultData;
+		console.log(jdata.length);
+
+		var i = 0;
+		state_name = state;
+		for (i; i < jdata.length; i++){
+			if (jdata[i].state == state_name) {
+				var colonies = jdata[i].colonies;
+				var loss = jdata[i].loss;
+			}
+		};
+
+		var tabledata = "<table> <tr>" + 
+		 "<th>State</th> <th>Number of bee colonies</th> <th>Percentage of colonies lost</th> </tr>" +
+		 "<tr> <td>" + state +  "</td>" + 
+		 "<td>" + colonies + "</td> <td>" + loss + "</td> </tr>" + 
+		 " </table>";
+
+		$('#beeTable').html(tabledata);
 		}
-	};
-
-	var tabledata = "<table> <tr>" + 
-	 "<th>State</th> <th>Number of bee colonies</th> <th>Percentage of colonies lost</th> </tr>" +
-	 "<tr> <td>" + state +  "</td>" + 
-	 "<td>" + colonies + "</td> <td>" + loss + "</td> </tr>" + 
-	 " </table>";
-
-	$('#beeTable').html(tabledata);
+	})
 }
+
+// function stateOutput(state) {
+// 	var i = 0;
+// 	// var beedata2 = processData();
+// 	console.log(beedata2);
+// 	state_name = state;
+// 	for (i; i < beedata2.length; i++){
+// 		if (beedata2[i].state == state_name) {
+// 			var colonies = beedata2[i].colonies;
+// 			var loss = beedata2[i].loss;
+// 		}
+// 	};
+
+// 	var tabledata = "<table> <tr>" + 
+// 	 "<th>State</th> <th>Number of bee colonies</th> <th>Percentage of colonies lost</th> </tr>" +
+// 	 "<tr> <td>" + state +  "</td>" + 
+// 	 "<td>" + colonies + "</td> <td>" + loss + "</td> </tr>" + 
+// 	 " </table>";
+
+// 	$('#beeTable').html(tabledata);
+// }
